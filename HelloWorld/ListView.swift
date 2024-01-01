fdc193c539ccf6873bdc19c24b0e190b019bc6cb
PieterNatanael <pieternatanael@icloud.com>
2024-01-01 15:40:00 +0800

    Create README.md

--- HelloWorld/ListView.swift
+++ HelloWorld/ListView.swift
@@ -1,18 +1,18 @@
 //
-//  NavigationView.swift
+//  ListView.swift
 //  HelloWorld
 //
-//  Created by Pieter Yoshua Natanael on 29/12/23.
+//  Created by Pieter Yoshua Natanael on 01/01/24.
 //
 
 import SwiftUI
 
-struct NavigationView: View {
+struct ListView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    NavigationView()
+    ListView()
 }
---
+++ HelloWorld/NavigationView1.swift
@@ -0,0 +1,19 @@
+//
+//  NavigationView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 29/12/23.
+//
+
+import SwiftUI
+
+struct NavigationView: View {
+    var body: some View {
+        NavigationView(content: {Text("Navigate") })
+    
+    }
+}
+
+#Preview {
+    NavigationView()
+}
