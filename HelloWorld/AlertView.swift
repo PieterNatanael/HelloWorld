801726d754fc2746678e3055093c46560e75449b
PieterNatanael <pieternatanael@icloud.com>
2024-01-03 14:35:48 +0800

    Create README.md

---
+++ HelloWorld/AlertView.swift
@@ -0,0 +1,18 @@
+//
+//  AlertView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 03/01/24.
+//
+
+import SwiftUI
+
+struct AlertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlertView()
+}
