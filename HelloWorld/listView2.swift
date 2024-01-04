b270bf734f0d8c7671afc284a995b983f220a810
PieterNatanael <pieternatanael@icloud.com>
2024-01-04 09:32:22 +0800

    Create README.md

---
+++ HelloWorld/listView2.swift
@@ -0,0 +1,18 @@
+//
+//  listView2.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 04/01/24.
+//
+
+import SwiftUI
+
+struct listView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    listView2()
+}
