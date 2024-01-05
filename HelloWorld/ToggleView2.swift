7e3a64c403f26f7a3c95545ee6feb83ac4ac30e2
PieterNatanael <pieternatanael@icloud.com>
2024-01-05 15:52:12 +0800

    Create README.md

---
+++ HelloWorld/ToggleView2.swift
@@ -0,0 +1,18 @@
+//
+//  ToggleView2.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 04/01/24.
+//
+
+import SwiftUI
+
+struct ToggleView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToggleView2()
+}
