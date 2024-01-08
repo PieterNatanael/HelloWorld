3cc7dce8285345f0c93ca71a807d09b8ba8287da
PieterNatanael <pieternatanael@icloud.com>
2024-01-08 15:04:50 +0800

    Create README.md

---
+++ HelloWorld/PickerView.swift
@@ -0,0 +1,18 @@
+//
+//  PickerView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 06/01/24.
+//
+
+import SwiftUI
+
+struct PickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickerView()
+}
---
+++ HelloWorld/StepperView.swift
@@ -0,0 +1,18 @@
+//
+//  StepperView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 08/01/24.
+//
+
+import SwiftUI
+
+struct StepperView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StepperView()
+}
