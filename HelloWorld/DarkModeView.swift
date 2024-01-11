e40c10053d20853b9973485c4097ddd2fcedf9fb
PieterNatanael <pieternatanael@icloud.com>
2024-01-11 14:51:36 +0800

    Create README.md

---
+++ HelloWorld/DarkModeView.swift
@@ -0,0 +1,18 @@
+//
+//  DarkModeView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 11/01/24.
+//
+
+import SwiftUI
+
+struct DarkModeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DarkModeView()
+}
