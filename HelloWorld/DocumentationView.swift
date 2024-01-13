c5312a3bf812f1a199f2bbc401d6d059a289d535
PieterNatanael <pieternatanael@icloud.com>
2024-01-13 10:51:40 +0800

    Create README.md

---
+++ HelloWorld/DocumentationView.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentationView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 13/01/24.
+//
+
+import SwiftUI
+
+struct DocumentationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentationView()
+}
