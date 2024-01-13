a10317932a5c9fc1e2bf4cb57a2fe7ba759643c4
PieterNatanael <pieternatanael@icloud.com>
2024-01-13 16:48:27 +0800

    Create README.md

---
+++ HelloWorld/onAppearView.swift
@@ -0,0 +1,18 @@
+//
+//  onAppearView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 13/01/24.
+//
+
+import SwiftUI
+
+struct onAppearView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    onAppearView()
+}
