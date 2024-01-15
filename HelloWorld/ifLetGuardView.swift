8b1129dfe8a051f66258c3851d8e6b67e944cf3d
PieterNatanael <pieternatanael@icloud.com>
2024-01-15 18:10:19 +0800

    Create README.md

---
+++ HelloWorld/ifLetGuardView.swift
@@ -0,0 +1,18 @@
+//
+//  ifLetGuardView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 15/01/24.
+//
+
+import SwiftUI
+
+struct ifLetGuardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ifLetGuardView()
+}
