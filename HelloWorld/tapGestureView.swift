db17225ef4a8fa3eeab1e5656f7d147c6e7fcaac
PieterNatanael <pieternatanael@icloud.com>
2024-01-16 09:29:32 +0800

    Create README.md

---
+++ HelloWorld/tapGestureView.swift
@@ -0,0 +1,18 @@
+//
+//  tapGestureView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 16/01/24.
+//
+
+import SwiftUI
+
+struct tapGestureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    tapGestureView()
+}
