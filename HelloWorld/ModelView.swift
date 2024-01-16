915e511b1f7c82c5d858b5eda034be2158c8cbbb
PieterNatanael <pieternatanael@icloud.com>
2024-01-16 10:21:09 +0800

    Create README.md

---
+++ HelloWorld/ModelView.swift
@@ -0,0 +1,18 @@
+//
+//  ModelView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 16/01/24.
+//
+
+import SwiftUI
+
+struct ModelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModelView()
+}
