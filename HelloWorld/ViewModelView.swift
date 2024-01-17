4491100d514d37976c26f0cdd4cad465796cc995
PieterNatanael <pieternatanael@icloud.com>
2024-01-17 14:57:48 +0800

    Create README.md

---
+++ HelloWorld/ViewModelView.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModelView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 17/01/24.
+//
+
+import SwiftUI
+
+struct ViewModelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewModelView()
+}
