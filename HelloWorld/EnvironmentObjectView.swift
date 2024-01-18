7554e43d0a0299244bd56e135576e11e5eaec5db
PieterNatanael <pieternatanael@icloud.com>
2024-01-18 10:37:42 +0800

    Create README.md

---
+++ HelloWorld/EnvironmentObjectView.swift
@@ -0,0 +1,18 @@
+//
+//  EnvironmentObjectView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 18/01/24.
+//
+
+import SwiftUI
+
+struct EnvironmentObjectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EnvironmentObjectView()
+}
