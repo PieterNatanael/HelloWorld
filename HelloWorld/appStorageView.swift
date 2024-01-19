e40e4a15ba551be858d52ce70688ca4551d034d3
PieterNatanael <pieternatanael@icloud.com>
2024-01-19 10:18:59 +0800

    Create README.md

---
+++ HelloWorld/appStorageView.swift
@@ -0,0 +1,18 @@
+//
+//  appStorageView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 19/01/24.
+//
+
+import SwiftUI
+
+struct appStorageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    appStorageView()
+}
