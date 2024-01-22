0537fc0a01d50d01777739d96e9f05222131ee26
PieterNatanael <pieternatanael@icloud.com>
2024-01-22 17:01:44 +0800

    Create README.md

---
+++ HelloWorld/onboardingView/onBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  onBoardingView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 22/01/24.
+//
+
+import SwiftUI
+
+struct onBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    onBoardingView()
+}
