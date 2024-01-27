9647982cfe3f149593b07cc1092be8a51235b64a
PieterNatanael <pieternatanael@icloud.com>
2024-01-27 15:19:08 +0800

    Create README.md

---
+++ HelloWorld/onboardingView/profileView.swift
@@ -0,0 +1,18 @@
+//
+//  profileView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 27/01/24.
+//
+
+import SwiftUI
+
+struct profileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    profileView()
+}
