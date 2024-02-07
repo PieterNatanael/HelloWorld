9de5dcd986cc9920acff1135f08a709f2a52dd7c
PieterNatanael <pieternatanael@icloud.com>
2024-02-07 11:18:28 +0800

    Create README.md

---
+++ HelloWorld/navigationStackView.swift
@@ -0,0 +1,18 @@
+//
+//  navigationStackView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 07/02/24.
+//
+
+import SwiftUI
+
+struct navigationStackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    navigationStackView()
+}
