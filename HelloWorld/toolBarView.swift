cb8e057888ad7d10a73207f83f6e961d39fb8502
PieterNatanael <pieternatanael@icloud.com>
2024-02-10 11:55:33 +0800

    Create README.md

---
+++ HelloWorld/toolBarView.swift
@@ -0,0 +1,18 @@
+//
+//  toolBarView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 10/02/24.
+//
+
+import SwiftUI
+
+struct toolBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    toolBarView()
+}
