52de36d977770ab6dbbb27312f8651b40edbc33d
PieterNatanael <pieternatanael@icloud.com>
2024-02-14 15:30:38 +0800

    Create README.md

---
+++ HelloWorld/resizeableSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  resizeableSheetView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 14/02/24.
+//
+
+import SwiftUI
+
+struct resizeableSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    resizeableSheetView()
+}
