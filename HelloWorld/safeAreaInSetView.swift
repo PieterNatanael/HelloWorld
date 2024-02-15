7de749a8c6a2d9f9999a8253eeee04347ffc5a9b
PieterNatanael <pieternatanael@icloud.com>
2024-02-15 15:40:48 +0800

    Create README.md

---
+++ HelloWorld/safeAreaInSetView.swift
@@ -0,0 +1,18 @@
+//
+//  safeAreaInSetView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 15/02/24.
+//
+
+import SwiftUI
+
+struct safeAreaInSetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    safeAreaInSetView()
+}
