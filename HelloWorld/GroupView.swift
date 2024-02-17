8b8159e4bf94abd23e0c18071ab12d962a84bcdd
PieterNatanael <pieternatanael@icloud.com>
2024-02-17 12:02:55 +0800

    Create README.md

---
+++ HelloWorld/GroupView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 17/02/24.
+//
+
+import SwiftUI
+
+struct GroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupView()
+}
