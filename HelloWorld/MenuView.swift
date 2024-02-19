2a74921896466ef029ac7ca745f40fd8e79400c9
PieterNatanael <pieternatanael@icloud.com>
2024-02-19 14:27:35 +0800

    Create README.md

---
+++ HelloWorld/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 19/02/24.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
---
+++ HelloWorld/animationUpdateView.swift
@@ -0,0 +1,18 @@
+//
+//  animationUpdateView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 17/02/24.
+//
+
+import SwiftUI
+
+struct animationUpdateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    animationUpdateView()
+}
