77600a8a9c9650955235bac95df744817c653bd0
PieterNatanael <pieternatanael@icloud.com>
2024-02-20 09:40:19 +0800

    Create README.md

---
+++ HelloWorld/nativePopoverView.swift
@@ -0,0 +1,18 @@
+//
+//  nativePopoverView.swift
+//  HelloWorld
+//
+//  Created by Pieter Yoshua Natanael on 20/02/24.
+//
+
+import SwiftUI
+
+struct nativePopoverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    nativePopoverView()
+}
